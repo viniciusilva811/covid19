50b15430b30a616f980ba5a9f188b96dea147dd5
Vinicius <vinisilva45@live.com>
2020-11-03 18:06:27 -0300

    Usando os dados de uma API.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App() {
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyle />
-      <div>
-        teste
-      </div>
       <Main />
     </StylesProvider>
   );
